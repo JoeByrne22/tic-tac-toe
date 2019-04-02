c8f99d57e9cca336e186c62b906c2a6e590e0a91
Joe Byrne <joesphbyrne2@gmail.com>
2019-04-02 19:19:56 +0100

    pictures adding, animation and winLogic

--- development/code-test/script.js
+++ development/code-test/script.js
@@ -2,6 +2,7 @@
 // const clickButtonBlue = document.getElementById('buttonBlue');
 // const test = document.querySelectorAll('button');
 const click = document.querySelectorAll('button');
+const container = document.getElementById('container');
 
 
 
@@ -15,29 +16,31 @@ const yellowSquares = [];
 
 for (let i = 0; i < click.length; i++) {
   click[i].addEventListener('click', () => {
-    clickCount.push('clicked');
-    if ( clickCount.length % 2 === 0 ) {
-      // red
-      if (event.target.classList.length === 4) {
-        console.log('already clicked');
+    if ( event.target.classList.length === 3 ) {
+      clickCount.push('clicked');
+      if ( clickCount.length % 2 === 0 ) {
+        // red
+        if (event.target.classList.length === 4) {
+          console.log('already clicked');
+        } else {
+          console.log('Move to clicked section');
+          event.target.classList.add('isActive', 'O');
+          click[i].style.backgroundColor ='#FF0000';
+          redSquares.push(i);
+          winLogicRed();
+        }
       } else {
-        console.log('Move to clicked section');
-        event.target.classList.add('isActive');
-        click[i].style.backgroundColor ='#FF0000';
-        redSquares.push(i);
+        //yellow
+        if (event.target.classList.length === 4) {
+          console.log('already clicked');
+        } else {
+          console.log('Move to clicked section');
+          event.target.classList.add('isActive', 'X');
+          // click[i].style.backgroundColor ='#FFFF00';
+          yellowSquares.push(i);
+          winLogicYellow();
+        }
       }
-      winLogicRed();
-    } else {
-      //yellow
-      if (event.target.classList.length === 4) {
-        console.log('already clicked');
-      } else {
-        console.log('Move to clicked section');
-        event.target.classList.add('isActive');
-        click[i].style.backgroundColor ='#FFFF00';
-        yellowSquares.push(i);
-      }
-      winLogicYellow();
     }
   });
 }
@@ -46,42 +49,43 @@ for (let i = 0; i < click.length; i++) {
 // Win Logic:
 function winLogicRed() {
   if ( redSquares.includes(0) && redSquares.includes(1) && redSquares.includes(2) ) {
-    alert('The winner is RED');
+    container.classList.add('Ypix');
   } else if ( redSquares.includes(3) && redSquares.includes(4) && redSquares.includes(5) ) {
-    alert('The winner is RED');
+    container.classList.add('Ypix');
   } else if ( redSquares.includes(6) && redSquares.includes(7) && redSquares.includes(8) ) {
-    alert('The winner is RED');
+    container.classList.add('Ypix');
   } else if ( redSquares.includes(0) && redSquares.includes(3) && redSquares.includes(6) ) {
-    alert('The winner is RED');
+    container.classList.add('Ypix');
   } else if ( redSquares.includes(1) && redSquares.includes(4) && redSquares.includes(7) ) {
-    alert('The winner is RED');
+    container.classList.add('Ypix');
   } else if ( redSquares.includes(2) && redSquares.includes(5) && redSquares.includes(8) ) {
-    alert('The winner is RED');
+    container.classList.add('Ypix');
   } else if ( redSquares.includes(0) && redSquares.includes(4) && redSquares.includes(8) ) {
-    alert('The winner is RED');
+    container.classList.add('Ypix');
   } else if ( redSquares.includes(2) && redSquares.includes(4) && redSquares.includes(6) ) {
-    alert('The winner is RED');
+    container.classList.add('Ypix');
   }
 }
 
 
 function winLogicYellow() {
   if ( yellowSquares.includes(0) && yellowSquares.includes(1) && yellowSquares.includes(2) ) {
-    alert('The winner is YELLOW');
+    container.classList.add('Xpix');
+    // container.classList.add('Xpix');
   } else if ( yellowSquares.includes(3) && yellowSquares.includes(4) && yellowSquares.includes(5) ) {
-    alert('The winner is YELLOW');
+    container.classList.add('Xpix');
   } else if ( yellowSquares.includes(6) && yellowSquares.includes(7) && yellowSquares.includes(8) ) {
-    alert('The winner is YELLOW');
+    container.classList.add('Xpix');
   } else if ( yellowSquares.includes(0) && yellowSquares.includes(3) && yellowSquares.includes(6) ) {
-    alert('The winner is YELLOW');
+    container.classList.add('Xpix');
   } else if ( yellowSquares.includes(1) && yellowSquares.includes(4) && yellowSquares.includes(7) ) {
-    alert('The winner is YELLOW');
+    container.classList.add('Xpix');
   } else if ( yellowSquares.includes(2) && yellowSquares.includes(5) && yellowSquares.includes(8) ) {
-    alert('The winner is YELLOW');
+    container.classList.add('Xpix');
   } else if ( yellowSquares.includes(0) && yellowSquares.includes(4) && yellowSquares.includes(8) ) {
-    alert('The winner is YELLOW');
+    container.classList.add('Xpix');
   } else if ( yellowSquares.includes(2) && yellowSquares.includes(4) && yellowSquares.includes(6) ) {
-    alert('The winner is YELLOW');
+    container.classList.add('Xpix');
   }
 }
 
